23a0f67f43f9c6eb3f69c0d225c5306618fe54a1
Dale Hamel <dale.hamel@shopify.com>
2019-12-28 13:15:25 -0500

    Add cold boot helper for llvm

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 STATIC_LINKING=${STATIC_LINKING:-OFF}
 STATIC_LIBC=${STATIC_LIBC:-OFF}
@@ -8,6 +9,26 @@ EMBED_LLVM=${EMBED_LLVM:-OFF}
 EMBED_CLANG=${EMBED_CLANG:-OFF}
 LLVM_VERSION=${LLVM_VERSION:-8} # default llvm to latest version
 RUN_TESTS=${RUN_TESTS:-1}
+
+# If running on Travis, we may need several builds incrementally building up
+# the cache in order to cold-start the build cache within the 50 minute travis
+# job timeout. The gist is to kill the job safely
+with_timeout()
+{
+
+  # FIXME detect and only do this on travis, else just call original
+  timeout 2400 $@
+  rc=$?
+
+  # TODO dalehamel
+  # The drawback of this is that it inappropriately sets the overall build
+  # to passing, perhaps another job can check if the cache was cold and fail
+  # if a retry occurs, to keep CI verity?
+  if [[ $rc == 124 ]];then
+    echo "Exiting early on timeout to upload cache and retry..."
+    exit 0
+  fi
+}
 
 # Build bpftrace
 mkdir -p "$1"
@@ -19,8 +40,8 @@ shift 2
 
 # It is necessary to build embedded llvm and clang targets first,
 # so that their headers can be referenced
-[[ $EMBED_LLVM  = "ON" ]] && make embedded_llvm "$@"
-[[ $EMBED_CLANG = "ON" ]] && make embedded_clang "$@"
+[[ $EMBED_LLVM  = "ON" ]] && with_timeout make embedded_llvm "$@"
+[[ $EMBED_CLANG = "ON" ]] && with_timeout make embedded_clang "$@"
 make "$@"
 
 if [ $RUN_TESTS = 1 ]; then
